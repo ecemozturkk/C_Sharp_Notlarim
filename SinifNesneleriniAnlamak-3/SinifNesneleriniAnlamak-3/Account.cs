f2b0eca15d8680094727052b91f7c3bbdda0a609
ecemozturkk <ecemozturk704@gmail.com>
2021-11-19 20:38:33 +0300

    Add files via upload

---
+++ SinifNesneleriniAnlamak-3/SinifNesneleriniAnlamak-3/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinifNesneleriniAnlamak_3
+{
+    class Account
+    {
+        public Account()   // Constructor (Yapici Metot) --> geri donusleri (void, int vs..) yoktur ve sinif adiyla=(Kutu) ayni adi alirlar
+        {
+
+        }
+        public string GetAccount(string userName)
+        {
+            if (!string.IsNullOrEmpty(userName)) //username null ya da bos degilse
+            {
+
+            }
+            if (!string.IsNullOrWhiteSpace(userName)) //username bosluk degilse
+            {
+
+            }
+            return "BILINMIYOR";
+        }
+        public string GetAccount(int userId)
+        {
+            string result = string.Empty;
+
+            if (userId > 0)
+            {
+                result = "Ali Kaya";
+            }
+            else
+            {
+                throw new Exception("Hata!");
+            }
+            return result;
+        }
+    }
+}
---
+++ SinifNesneleriniAnlamak-3/SinifNesneleriniAnlamak-3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SinifNesneleriniAnlamak_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            var result = acc.GetAccount(3);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
