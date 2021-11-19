d78b2488f0bb3522b021879231ba4681c36293c7
ecemozturkk <ecemozturk704@gmail.com>
2021-11-19 20:36:48 +0300

    Add files via upload

---
+++ SinifNesneleriniAnlamak-1/SinifNesneleriniAnlamak-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SinifNesneleriniAnlamak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SINIFLAR
+
+            Kutu k = new Kutu(5,6,7);    // () konmasinin sebebi constructor methods (yapici metot)
+            Console.WriteLine(k.uzunluk);
+        }
+    }
+}
