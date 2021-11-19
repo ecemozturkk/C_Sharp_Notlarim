688e55470f48dee8b8b16e619eff0ccfc5ee7e9a
ecemozturkk <ecemozturk704@gmail.com>
2021-11-19 20:31:53 +0300

    Add files via upload

---
+++ 60.Ic_Ice_Sinif_Kullanimi/60.Ic_Ice_Sinif_Kullanimi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60.Ic_Ice_Sinif_Kullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.PropA = 4;
+
+            A.B b = new A.B();
+            b.PropB = 5;
+
+            Console.ReadLine();
+
+        }
+    }
+    class A
+    {
+        public int PropA { get; set; }
+
+        public A()
+        {
+            Console.WriteLine("A sinifi");
+        }
+
+        public class B
+        {
+            public int PropB { get; set; }
+            public B()
+            {
+                Console.WriteLine("B sinifi");
+            }
+        }
+       
+    }
+}
+
